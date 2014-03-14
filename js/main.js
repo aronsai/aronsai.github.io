457963a56d877a5beba1a5f92e174d0d61735a07
aronsai <aron.sai@gmail.com>
2014-03-14 23:35:24 +0100

    Version 0.6
    Ny design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+
+	$("#projectbutton").hover(
+		function(){
+			$("#projectbutton a").stop().fadeTo('fast', 1;)
+		},
+		function(){
+			$("#projectbutton a").stop().fadeTo('fast', 0;)
+		});
+
 });
