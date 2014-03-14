b63bb543b22e627c0aaef25d49d4d0556c2e6f2e
aronsai <aron.sai@gmail.com>
2014-03-15 00:39:46 +0100

    0.6.3.1
    small changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
+
 	$("#projectbutton").click(function() {
 		$('html, body').animate({
         scrollTop: $("#block2").offset().top
     	}, 500);
-	})
+	});
+
+	$("#aboutbutton").click(function() {
+		$('html, body').animate({
+        scrollTop: $("#block3").offset().top
+    	}, 500);
+	});
+
+	$("#contactbutton").click(function() {
+		$('html, body').animate({
+        scrollTop: $("#block4").offset().top
+    	}, 500);
+	});
+
+	$("#linkpuff").click(function() {
+		$('html, body').animate({
+        scrollTop: $("#block4").offset().top
+    	}, 500);
+	});
+
 });
